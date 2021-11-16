b59d2a3a89b83cc3a905404eb3944c48e0dbea01
Andrei <6abc6@mail.ru>
2021-11-16 13:00:23 +0300

    add doenloadcategories

--- OurPlanet/CategoriesViewController.swift
+++ OurPlanet/CategoriesViewController.swift
@@ -6,7 +6,6 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
   let categories = BehaviorRelay<[EOCategory]>(value: [])
   let disposeBag = DisposeBag()
   
-  
   @IBOutlet var tableView: UITableView!
   
   override func viewDidLoad() {
@@ -25,7 +24,23 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
   
   func startDownload() {
     let eoCategories = EONET.categories
+    let downloadEvents = EONET.events(forLast: 360)
     eoCategories
+      .bind(to: categories)
+      .disposed(by: disposeBag)
+    let updateCategories = Observable
+      .combineLatest(eoCategories, downloadEvents) {
+        (categories, events) -> [EOCategory] in
+        return categories.map { category in
+          var cat = category
+          cat.events = events.filter {
+            $0.categories.contains(where: { $0.id == category.id })
+          }
+          return cat
+        }
+      }
+    eoCategories
+      .concat(updateCategories)
       .bind(to: categories)
       .disposed(by: disposeBag)
   }
@@ -33,14 +48,11 @@ class CategoriesViewController: UIViewController, UITableViewDataSource, UITable
   // MARK: UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-    
-    
     return categories.value.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
-    
     let category = categories.value[indexPath.row]
     cell.textLabel?.text = category.name
     cell.detailTextLabel?.text = category.description
